d2453095342e061f76f8bb3e7e04c49abcd05d62
Praneeth Maldakal <vengalreddysushmitha@gmail.com>
2022-09-10 17:40:30 +0530

    [SUSHMITHA]ADDED:Refactor the message using construccter

--- MoodAnalyser/MoodAnalyzer/src/test/java/com/bridzelab/MoodAnalyzer/TestMoodAnalyzer.java
+++ MoodAnalyser/MoodAnalyzer/src/test/java/com/bridzelab/MoodAnalyzer/TestMoodAnalyzer.java
@@ -3,29 +3,27 @@ package com.bridzelab.MoodAnalyzer;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-    public class TestMoodAnalyzer
-    {
+public class TestMoodAnalyzer{
+
 
     @Test
-        public void whenMoodIsSad()
-        {
-
-            //TC 1.1 "I am in sad mood"
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-        String mood = moodAnalyzer.moodCheck("I am in sad mood");
-            System.out.println(mood);
-            Assertions.assertEquals(mood, "SAD");
-        }
-        @Test
-        
-        public void whenMoodIsHappy()
-        {
-
-            //TC 1.2 "I am in happy mood"
-            MoodAnalyzer moodAnalyzer = new MoodAnalyzer();
-            String mood = moodAnalyzer.moodCheck("I am in happy mood");
-            System.out.println(mood);
-            Assertions.assertEquals(mood, "HAPPY");
-        }
+    public void whenMoodIsSad()
+    {
+        //Tc1.1 Refactor
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a sad message");
+        String mood = moodAnalyzer.analyseMood();
+        System.out.println("mood");
+        Assertions.assertEquals(mood,"SAD");
     }
 
+    @Test
+    public void whenMoodIsHappy()
+    {
+        //Tc1.2 Refactor
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a happy message");
+        String mood1 = moodAnalyzer.analyseMood();
+        System.out.println("mood1");
+        Assertions.assertEquals(mood1,"HAPPY");
+
+    }
+}
